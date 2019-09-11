df796bd62266e5ed6ed8793239cad360c2a0bf10
Jordan M <github@defestri.org>
2019-09-11 20:31:23 +1000

    Update Vault ACL as per the docs

--- consul/base-acls/vault_storage.tf
+++ consul/base-acls/vault_storage.tf
@@ -1,7 +1,21 @@
 resource "consul_acl_policy" "vault-storage" {
-  name  = "vault-storage"
+  name = "vault-storage"
+  # Rules as per
+  # https://www.vaultproject.io/docs/configuration/storage/consul.html
   rules = <<-RULE
     key_prefix "vault/" {
+      policy = "write"
+    },
+    node_prefix "" {
+      policy = "write"
+    },
+    service "vault" {
+      policy = "write"
+    },
+    agent_prefix "" {
+      policy = "write"
+    },
+    session_prefix "" {
       policy = "write"
     }
     RULE
